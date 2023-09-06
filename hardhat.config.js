99071c195fd20df9a84e830b80e713068dda643a
Austin <austin@Austinui-MacBookPro.local>
2023-09-06 13:54:13 +0900

    mod env

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
     solidity: "0.8.19",
     networks: {
         sepolia: {
-            url: process.env.SEPOLIA_RPC_URL,
+            url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
             accounts: [process.env.PRIVATE_KEY],
             blockConfirmations: 6,
         },
